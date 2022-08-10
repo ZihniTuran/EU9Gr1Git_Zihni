4ad5dfbfb61876a5ed3be020870e9640bf326bd7
Zihni Turan <zihni@gmx.net>
2022-08-10 20:47:53 +0300

    second commit from Zihni

--- src/Zihni.java
+++ src/Zihni.java
@@ -1,5 +1,6 @@
 public class Zihni {
     public static void main(String[] args) {
         System.out.println("First Change from zihni");
+        System.out.println("second change from Zihni");
     }
 }
